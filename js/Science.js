57fef03b7ec1af05f906c78f4a01acb95b7cf498
John Paul Pineda <57859589+ijohnpaul2000@users.noreply.github.com>
2021-10-10 18:07:05 +0800

    FInal

---
+++ js/Science.js
@@ -0,0 +1,125 @@
+const quiz = [
+    {
+        // Number 1
+        q:'Which famous British physicist wrote A Brief History of Time?',
+        options:[
+            'Stephen Hawking',
+            'Michal Faraday',
+            'J. J. Thomson',
+            'Isaac Newton'
+        ],
+        answer:0
+    },
+    {
+        // Number 2
+        q:'At what temperature are Celsius and Fahrenheit equal?',
+        options:[
+            '-40',
+            '-34',
+            '-33',
+            '-32'
+        ],
+        answer:0
+    },
+    {
+        // Number 3
+        q:'What modern-day country was Marie Curie born in?',
+        options:[
+            'Poland',
+            'Belarus',
+            'Germany',
+            'Ukraine'
+        ],
+        answer:0
+    },
+    {
+        // Number 4
+        q:'What is the biggest planet in our solar system?',
+        options:[
+            'Jupiter',
+            'Neptune',
+            'Saturn',
+            'Uranus'
+        ],
+        answer:0
+    },
+    {
+        // Number 5
+        q:'How many vertebrae does the average human possess?',
+        options:[
+            '33',
+            '23',
+            '22',
+            '34'
+        ],
+        answer:0
+        
+    }
+    ,
+    {
+        // Number 6
+        q:'What was the name of the first man-made satellite launched by the Soviet Union in 1957?',
+        options:[
+            'Sputnik 1',
+            'Explorer 1',
+            'Skylab',
+            'Vanguard 1'
+        ],
+        answer:0
+        
+    }
+    ,
+    {
+        // Number 7
+        q:'Which oath of ethics taken by doctors is named after an Ancient Greek physician?',
+        options:[
+            'Hippocratic Oath',
+            'Oath of allegiance',
+            'Paupers Oath',
+            'Ironclad Oath'
+        ],
+        answer:0
+        
+    }
+    ,
+    {
+        // Number 8
+        q:'What kind of energy does an unlit match have?',
+        options:[
+            'Thermal Energy',
+            'Chemical Energy',
+            'Kinetic Energy',
+            'Radiant Energy'
+        ],
+        answer:0
+        
+    }
+    ,
+    {
+        // Number 9
+        q:'Which Apollo moon mission was the first to carry a lunar rover?',
+        options:[
+            'Apollo 15',
+            'Saturn V',
+            'Saturn IB',
+            'Apollo TY'
+        ],
+        answer:0
+        
+    }
+    ,
+    {
+        // Number 10
+        q:'How many teeth does an adult human have?',
+        options:[
+            '32',
+            '37',
+            '33',
+            '35'
+        ],
+        answer:0
+        
+    }
+    
+    ];
+    
